281501be305d296757cf1bc844558ae7d4360308
Yudhi Oktavian <yudhioktavian@icloud.com>
2021-01-16 02:25:34 +0800

    fix cetakan

--- resources/js/komponen/anggota/printing.js
+++ resources/js/komponen/anggota/printing.js
@@ -8,53 +8,60 @@ export default class PrintArea{
             "left=0,top=0,width=480,height=320,toolbar=0,scrollbars=0,status=0"
         );
         printArea.document.write(`
-            <table width='420px' border='0'>
-                <tr>
-                    <th style='background:#c0392b;color:white'>
-                        KARTU ANGGOTA
-                    </th>
-                </tr>
-                <tr>
-                    <th style='text-align:left'>
-                        Nama
-                    </th>
-                </tr>
-                <tr>
-                    <td>
-                        ${data.name}
-                    </td>
-                </tr>
-                <tr>
-                    <th style='text-align:left'>
-                        E-Mail
-                    </th>
-                </tr>
-                <tr>
-                    <td>
-                        ${data.email}
-                    </td>
-                </tr>
-                <tr>
-                    <th style='text-align:left'>
-                        Alamat
-                    </th>
-                </tr>
-                <tr>
-                    <td>
-                        ${data.alamat}
-                    </td>
-                </tr>
-                <tr>
-                    <th style='text-align:left'>
-                        Telepon
-                    </th>
-                </tr>
-                <tr>
-                    <td>
-                        ${data.telepon}
-                    </td>
-                </tr>
-            </table>
+            <html>
+                <head>
+                    <title>Kartu Anggota</title>
+                </head>
+                <body style='padding:2px;margin:0'>
+                    <table cellpadding='0' cellspacing='0' width='420px' border='0' style='border:1px black solid;border-radius:5px;background:#c0392b;'>
+                        <tr>
+                            <th style='background:#c0392b;color:white;padding:15px'>
+                                KARTU ANGGOTA
+                            </th>
+                        </tr>
+                        <tr>
+                            <th style='text-align:left;padding:5px;background:white;'>
+                                Nama
+                            </th>
+                        </tr>
+                        <tr>
+                            <td style='background:white;padding:5px;'>
+                                ${data.name}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th style='text-align:left;padding:5px;background:white;'>
+                                E-Mail
+                            </th>
+                        </tr>
+                        <tr>
+                            <td style='background:white;padding:5px;'>
+                                ${data.email}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th style='text-align:left;padding:5px;background:white;'>
+                                Alamat
+                            </th>
+                        </tr>
+                        <tr>
+                            <td style='background:white;padding:5px;'>
+                                ${data.alamat}
+                            </td>
+                        </tr>
+                        <tr>
+                            <th style='text-align:left;padding:5px;background:white;'>
+                                Telepon
+                            </th>
+                        </tr>
+                        <tr>
+                            <td style='background:white;padding:5px;'>
+                                ${data.telepon}
+                            </td>
+                        </tr>
+                    </table>
+                </body>
+            </html>
         `);
         printArea.print();
     }
